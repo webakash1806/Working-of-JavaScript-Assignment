7125de0159a6b9492d98f1529b07fd32f749d9cc
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-04-12 12:56:11 +0530

    updated

---
+++ 4/script.js
@@ -0,0 +1,18 @@
+{
+    console.log(a)//undefined
+    console.log(b)//Reference Error
+    console.log(c)//Reference Error
+
+    // ++++++++++++++++++++++++++++++
+
+    let a = "Akash"
+    var b = 18
+    const c = 06
+
+    //+++++++++++++++++++++++++++++++
+
+    console.log(a)// Output : Akash
+    console.log(b)// Output : 18
+    console.log(c)// Output : 06
+
+}
