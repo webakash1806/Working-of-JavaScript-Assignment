26d9ffa19870ef9b203700952a955452c8faf1c0
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-04-12 12:37:10 +0530

    upadted

--- 2. multiply numbers/script.js
+++ 2. multiply numbers/script.js
@@ -1,7 +1,12 @@
 console.log(multiplyNumbers(10, 2))
 
+// we cannot access multiplyNumbers before initialization because it is witten in function expression for that we have to write it after initialization or we have to not use function expression
+
 const multiplyNumbers = function (a, b) {
     return a * b
 }
 
-console.log(multiplyNumbers(2, 2))
+
+
+
+
